11a4299e7dad230942915f68a02b93569726b2dc
MaxMEllon <mozi_kke@excite.co.jp>
2015-11-05 06:48:17 +0900

    :100: Add: SampleTest

---
+++ AigisCapture-Test/MainViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AigisCapture.ViewModel;
+using NUnit.Framework;
+
+namespace AigisCapture_Test
+{
+    [TestFixture]
+    public class MainViewModelTest
+    {
+        [Test]
+        public void Xのテスト()
+        {
+            MainViewModel mainVM = new MainViewModel();
+            mainVM.X = "111";
+            Assert.AreNotEqual(mainVM.X, "121");
+            Assert.AreEqual(mainVM.X, "111");
+        }
+    }
+}
